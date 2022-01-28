b5febe1cdbcdf84af461a993b336d98b461878ed
nar2p1nk <farrisisqandar@gmail.com>
2022-01-28 22:58:08 +0800

    finishing hash.js

--- hash.js
+++ hash.js
@@ -1,5 +1,20 @@
+// a hash is basically just an encryption without the decrypt
+
 const {createHash} = require('crypto');
 
+word = 'my name is walter hardwill white'
+
 function hasher(word){
+    const hashedWord =
+    // enter hashing algorithm
+        createHash('sha256')
+    // enter string to hash
+        .update(word)
+    // enter an output format, it may either be
+    // "binary", "hex" or "base64". It defaults to binary.
+        .digest('hex')
+    console.log(`word:${word}\nhash:${hashedWord}`)
 
 }
+
+hasher(word)
